c15b049c8041d699f685657aeb74ab3d5c7ee766
Johannes Weniger <jweniger2@gmail.com>
2022-07-21 12:00:28 +0100

    added error handling through a second async function run which calls the messageDiscord function in a try / catch block

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const axios = require('axios');
 
-async function messageDiscordChannel (input) {
-    if (!input.DISCORD_TOKEN) return console.error('No Discord Token provided, register a bot in the Discord Developer Portal (https://discord.com/developers/applications), grant OAuth access, invite it to your Discord server, and store the token as ENV DISCORD_TOKEN and load it in the function call');
-    if (!input.messageString) return console.error('No message string provided, please format your message as string and pass it in function call');
-    if (!input.channelId) return console.error('no channelID provided, please copy the channelId from Discord and pass it in the function call');
+async function messageDiscordChannel({DISCORD_TOKEN, messageString, channelId}) {
+    if (!DISCORD_TOKEN) throw('No DISCORD_TOKEN provided, register a bot in the Discord Developer Portal (https://discord.com/developers/applications), grant OAuth access, invite it to your Discord server, and store the token as ENV DISCORD_TOKEN and load it in the function call');
+    if (!messageString) throw('No messageString string provided, please format your message as string and pass it in function call');
+    if (!channelId) throw('no channelId provided, please copy the channelId from Discord and pass it in the function call');
 
 
-    const data = {"content": input.messageString}
-    const channelId = input.channelId
+    const data = {"content": messageString}
     const config = {
         headers: {
-            "authorization": 'Bot ' + input.DISCORD_TOKEN,
+            "authorization": 'Bot ' + DISCORD_TOKEN,
             "content-type": "application/json"
         }
     };
@@ -19,12 +18,18 @@ async function messageDiscordChannel (input) {
      const response =  await axios.post(`https://discord.com/api/v9/channels/${channelId}/messages`, data, config)
 
     } catch (error) {
-    throw (`Error: ${error.response.data.code} ${error.response.data.message} \nPlease check the Discord Documentation for further details https://discord.com/developers/docs/topics/opcodes-and-status-codes`);
+    throw new Error(`Error: ${error.response.data.code} ${error.response.data.message} \nPlease check the Discord Documentation for further details https://discord.com/developers/docs/topics/opcodes-and-status-codes`);
     }
 
 }
 
+async function run(input) {
+    try {
+        await messageDiscordChannel(input);
+    } catch (e) {
+        console.error(e);
+    }
+}
 
 
-
-module.exports = messageDiscordChannel;
+module.exports = run;
